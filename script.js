e5614ebc71af80601d21fac092131028348adc39
cubap <cubap@slu.edu>
2015-03-04 20:39:08 -0600

    nearly complete additions

--- script.js
+++ script.js
@@ -1,211 +1,156 @@
-angular.module("Eventities", ['ngStorage'])
-    .value("definedTypes", {
-        "ROOT": {
-            class: null,
-            requiredFields: ["label", "@id", "@type"],
-            defaultFields: {
-                label: "unlabelled",
-                "@id": "new",
-                "@type": "nv",
-                outcomes: [],
-                events: []
-            }
-        },
-        "ENTITY": {
-            class: "ROOT",
-            requiredFields: ["events"],
-            defaultFields: {
-                label: "new Entity",
-                "@type": "entity",
-                "events": ["DIGITALMINTINGDATE"]
-            }
-        },
-        "EVENT": {
-            class: "ROOT",
-            requiredFields: ["outcomes"],
-            defaultFields: {
-                label: "new Event",
-                "@type": "event"
-            }
-        },
-        "PERSON": {
-            class: "ENTITY",
-            defaultFields: {
-                label: "unnamed",
-                "@type": "person",
-                events: ["BIRTH"]
-            },
-            suggestedEvents: ["WEDDING", "RELOCATION", "DEATH", "BAPTISM"]
-        },
-        "BIRTH": {
-            class: "EVENT",
-            defaultFields: {
-                label: "birth",
-                "@type": "nv:birth",
-                date: "",
-                location: "",
-                outcomes: [{
-                        "rdf:describes": "+0", // argument index effected, create new if missing
-                        "foaf:dateOfBirth": "date|self", // property conferred from this event
-                        "hasMother": 1, // argument index effected, undefined if missing
-                        "hasFather": 2, // argument index effected
-                        "foaf:givenName": "",
-                        "foaf:familyName": "foaf:familyName|2" // in this case, take the father's name
-                    }, {
-                        "rdf:describes": 1,
-                        "motherTo": 0
-                    }, {
-                        "rdf:describes": 2,
-                        "fatherTo": 0
-                    }]
-            }
-        }
-    })
+angular.module("Eventities", ['ngStorage', 'ui.bootstrap'])
     .service("EntitiesService", function ($localStorage) {
-            var service = this;
-            var checkKnownTypes = function (type) {
-                var init = {};
-                while (types[type]) {
-                    var ext = angular.copy(types[type].defaultFields);
-                    if (ext.events && init.events) {
-                        init.events = init.events.concat(ext.events);
-                    }
-                    init = angular.extend(ext, init);
-                    type = types[type]["class"];
+        var service = this;
+        var checkKnownTypes = function (type) {
+            var init = {};
+            while (types[type]) {
+                var ext = angular.copy(types[type].defaultFields);
+                if (ext.events && init.events) {
+                    init.events = init.events.concat(ext.events);
                 }
-                return init;
-            };
-            this.new = function (init) {
-                var ent = checkKnownTypes(init["@type"]);
-                ent['@id'] = makeId(ent['@type']);
-                $localStorage.entities[ent['@id']] = ent;
-                ent.events = applyEvents(ent.events, init.participants);
-                return ent;
-            };
-            var makeId = function (t) {
-                var generator = "nv"; // Eventities
-                var type = t || "unknown";
-                var tail = Date.now();
-                return [generator, type, tail].join("-");
-            };
-            var applyEvents = function (events, participants) {
-                var evts = [];
-                // build events based on definedTypes
-                angular.forEach(events, function (ev) {
-                    if (types[ev]) {
-                        var type = angualr.copy(ev);
-                        while (types[type]) {
-                            var newEvt = service.new(types[type]);
+                init = angular.extend(ext, init);
+                type = types[type]["class"];
+            }
+            return init;
+        };
+        this.new = function (init) {
+            var ent = checkKnownTypes(init["@type"]);
+            ent['@id'] = makeId(ent['@type']);
+            $localStorage.entities[ent['@id']] = ent;
+            ent.events = applyEvents(ent.events, init.participants);
+            return ent;
+        };
+        var makeId = function (t) {
+            var generator = "nv"; // Eventities
+            var type = t || "unknown";
+            var tail = Date.now();
+            return [generator, type, tail].join("-");
+        };
+        var applyEvents = function (events, participants) {
+            var evts = [];
+            // build events based on definedTypes
+            angular.forEach(events, function (ev) {
+                if (types[ev]) {
+                    var type = angualr.copy(ev);
+                    while (types[type]) {
+                        var newEvt = service.new(types[type]);
 
 
 
-                        }
                     }
-                })
-                return evts;
-            };
-            $localStorage.$default({
+                }
+            })
+            return evts;
+        };
+        $localStorage.$default({
             entities: {},
             types: {},
-                "@context": {
-                    config: {
-                        "ENTITY": {
-                            class: null,
+            "@context": {
+                config: {
+                    "ENTITY": {
+                        class: null,
                         "@type": "nv:Entity",
                         requires: [],
-                            requiredFields: ["label", "@id", "@type"],
-                            defaultFields: [{
-                                    label: "label",
-                                    "@id": "rdfs:label",
-                                    _defaultValue: null,
-                                    _typeOf: "string", // ultimately resolves to
-                                    _nMin: 0,
-                                    _nMax: Infinity
-                                }, {
-                                    label: "@type",
-                                    "@id": "rdf:type",
-                                    _defaultValue: "@id",
-                                    _typeOf: "string,uri",
-                                    _nMin: 1,
-                                    _nMax: Infinity
-                                }]
-                        },
-                        "EVENT": {
-                            class: "ENTITY",
-                            "@type": "nv:Event",
-                            requiredFields: ["outcomes"],
-                            defaultFields: [{
-                                    label: "outcomes",
-                                    "@id": "aggregates",
-                                    _defaultValue: [],
-                                    _typeOf: ["OUTCOME"],
-                                    _nMin: 0,
-                                    _nMax: Infinity
-                                }]
-                        },
-                        "OUTCOME": {
-                            /* This is an annotation that applies a new property
-                             * to an Entity and is evidenced by an Event. The
-                             * target is the Entity being described and the body
-                             * is the property and value to be applied.
-                             * */
-                            class: null,
-                            "@type": ["nv:Outcome", "oa:Annotation"],
-                            requiredFields: ["hasTarget", "hasBody"],
-                            defaultFields: [{
-                                    label: "hasTarget",
-                                    "@id": "oa:hasTarget",
-                                    _defaultValue: "owl:Nothing", // Specifically targetting nothing
-                                    _typeOf: "ENTITY",
-                                    _nMin: 1,
-                                    _nMax: 1 // one at a time for now, the interface can simplify it
-                                }, {
-                                    label: "hasBody",
-                                    "@id": "oa:hasBody",
-                                    _defaultValue: "owl:Nothing",
-                                    _typeOf: "EFFECT", // the property and value to impose
-                                    _nMin: 1,
-                                    _nMax: 1
-                                }]
-                        },
-                        "EFFECT": {
-                            class: null,
-                            "@type": "@id",
-                            requiredFields: ["property", "evidence", "@value"],
-                            defaultFields: [{
-                                    label: "property",
-                                    "@id": "rdf:Property",
-                                    _defaultValue: undefined,
-                                    _typeOf: "string",
-                                    _nMin: 1,
-                                _nMax: 1,
-                                required: true
-                                }, {
-                                    label: "@value",
-                                    "@id": "rdf:value",
-                                    _defaultValue: null,
-                                    _typeOf: "*",
-                                    _nMin: 0,
-                                _nMax: Infinity,
-                                required: true
-                                }, {
-                                    label: "evidence",
-                                "@id": "oa:Composite", // Things to support the assertion property:@value
-                                _defaultValue: [],
-                                _typeOf: "*",
+                        defaultFields: [{
+                                label: "label",
+                                "@id": "rdfs:label",
+                                _defaultValue: null,
+                                _typeOf: ["string"], // ultimately resolves to
                                 _nMin: 0,
                                 _nMax: Infinity,
+                                _infinite: true,
                                 required: true
-                                }]
-                        }
+                            }, {
+                                label: "@type",
+                                "@id": "rdf:type",
+                                _defaultValue: "@id",
+                                _typeOf: ["string", "uri"],
+                                _nMin: 1,
+                                _nMax: Infinity,
+                                _infinite: true,
+                                required: true
+                            }]
+                    },
+                    "EVENT": {
+                        class: "ENTITY",
+                        "@type": "nv:Event",
+                        defaultFields: [{
+                                label: "outcomes",
+                                "@id": "aggregates",
+                                _defaultValue: [],
+                                _typeOf: ["OUTCOME"],
+                                _nMin: 0,
+                                _nMax: Infinity,
+                                _infinite: true,
+                                required: true
+                            }]
+                    },
+                    "OUTCOME": {
+                        /* This is an annotation that applies a new property
+                         * to an Entity and is evidenced by an Event. The
+                         * target is the Entity being described and the body
+                         * is the property and value to be applied.
+                         * */
+                        class: null,
+                        "@type": ["nv:Outcome", "oa:Annotation"],
+                        defaultFields: [{
+                                label: "hasTarget",
+                                "@id": "oa:hasTarget",
+                                _defaultValue: "owl:Nothing", // Specifically targetting nothing
+                                _typeOf: ["ENTITY"],
+                                _nMin: 1,
+                                _nMax: 1, // one at a time for now, the interface can simplify it
+                                required: true
+                            }, {
+                                label: "hasBody",
+                                "@id": "oa:hasBody",
+                                _defaultValue: "owl:Nothing",
+                                _typeOf: ["EFFECT"], // the property and value to impose
+                                _nMin: 1,
+                                _nMax: 1,
+                                required: true
+                            }]
+                    },
+                    "EFFECT": {
+                        class: null,
+                        "@type": "@id",
+                        defaultFields: [{
+                                label: "property",
+                                "@id": "rdf:Property",
+                                _defaultValue: undefined,
+                                _typeOf: ["string"],
+                                _nMin: 1,
+                                _nMax: 1,
+                                required: true
+                            }, {
+                                label: "@value",
+                                "@id": "rdf:value",
+                                _defaultValue: null,
+                                _typeOf: ["*"],
+                                _nMin: 0,
+                                _nMax: Infinity,
+                                _infinite: true,
+                                required: true
+                            }, {
+                                label: "evidence",
+                                "@id": "oa:Composite", // Things to support the assertion property:@value
+                                _defaultValue: [],
+                                _typeOf: ["*"],
+                                _nMin: 0,
+                                _nMax: Infinity,
+                                _infinite: true,
+                                required: true
+                            }]
                     }
                 }
-            });
-            this.list = $localStorage.entities;
-            this.resetStorage = $localStorage.reset;
-        })
+            }
+        });
+        this.list = $localStorage.entities;
+        this.resetStorage = $localStorage.reset;
+    })
     .controller("AdminController", function ($scope, $localStorage) {
         $scope.types = $localStorage.types;
+        $scope.defaultObjectTypes = ["string", "number", "uri", "array"];
         $scope.addToConfig = function (config) {
             var toAdd = config;
         };
@@ -214,21 +159,49 @@ angular.module("Eventities", ['ngStorage'])
             defaultFields: []
         };
         $scope.loadClassDefaults = function () {
-            angular.extend($scope.newEnt, $localStorage.types[$scope.newEnt.class]);
+            var holdClass = $scope.newEnt.class;
+            $scope.newEnt.defaultFields.length = 0;
+            angular.extend($scope.newEnt, angular.copy($localStorage.types[$scope.newEnt.class]));
+            $scope.newEnt.class = holdClass;
+            $scope.newEnt.defaultFields = $scope.newEnt.defaultFields.concat(inherit($scope.newEnt));
         };
-        $scope.toggleValue = function (value) {
-            if (angular.isArray($scope.newEnt.defaultFields)) {
-                $scope.newEnt.defaultFields = [$scope.newEnt.defaultFields];
-        }
-        var index = $scope.newEnt.defaultFields.indexOf(value);
-        if (index === -1) {
-                $scope.newEnt.defaultFields.push(value);
-    } else {
-                $scope.newEnt.defaultFields.splice(index, 1);
+        $scope.toggleValue = function (field, value) {
+            var index = field._typeOf.indexOf(value);
+            if (index === -1) {
+                field._typeOf.push(value);
+            } else {
+                field._typeOf.splice(index, 1);
             }
-            if ($scope.newEnt.defaultFields.length === 1) {
-                $scope.newEnt.defaultFields = $scope.newEnt.defaultFields[0];
+        };
+        var inherit = function (ent) {
+            var inh = [];
+            if (ent) {
+                var c = $localStorage.types[ent.class].class;
+                while (c) {
+                    var nf = $localStorage.types[c].defaultFields;
+                    for (var i = 0; i < nf.length; i++) {
+                        var found = false;
+                        var df = ent.defaultFields;
+                        for (var j = 0; j < df.length; j++) {
+                            if (nf[i].label === df[j].label) {
+                                found = true;
+                                break;
+                            }
+                        }
+                        for (var j = 0; j < inh.length; j++) {
+                            if (nf[i].label === inh[j].label) {
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            inh.push(nf[i]);
+                        }
+                    }
+                    c = $localStorage.types[c].class;
+                }
             }
+            return inh;
         };
     })
     .controller("MainController", function ($scope, $localStorage, EntitiesService) {
